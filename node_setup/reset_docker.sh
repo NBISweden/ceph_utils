2de8fca839e0c4a30d166575863ecf01874e25af
dahlo <m.dahlo@gmail.com>
2025-02-03 09:20:00 +0100

    Added script to reset a docker installation

---
+++ node_setup/reset_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+docker system prune -af --volumes
