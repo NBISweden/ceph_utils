3e3d93a9f45f4a1faea2ad9a40d728b7c516ace4
dahlo <m.dahlo@gmail.com>
2025-02-03 09:30:53 +0100

    harder reset

--- node_setup/reset_docker.sh
+++ node_setup/reset_docker.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
-docker volume rm $(docker volume ls -q)
-docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
-docker system prune -af --volumes
+# docker stop $(docker ps -aq)
+# docker rm $(docker ps -aq)
+# docker rmi $(docker images -q)
+# docker volume rm $(docker volume ls -q)
+# docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+# docker system prune -af --volumes
+
+
+systemctl stop docker
+rm -rf /var/lib/docker
+systemctl start docker
